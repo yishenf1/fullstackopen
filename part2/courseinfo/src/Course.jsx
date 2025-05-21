e2708deeedc44a2bedd2daf9d57e8b5f5c7f0114
yishenf1 <yishenfeng01@gmail.com>
2025-05-21 12:24:49 -0400

    Add courseinfo
    Exercise 2.1-2.5

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,36 @@
+﻿const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+    <div>
+        {props.parts.map(part => <Part part={part} key={part.id } />)}
+    </div>
+)
+
+const Part = (props) => (
+    <p>
+        {props.part.name} {props.part.exercises}
+    </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+
+const Course = (props) => {
+    return (
+        <div>
+            {props.courses.map(course => 
+                <div key={course.id}>
+                    <Header course={course.name} />
+                    <Content parts={course.parts} />
+                    <Total total={course.parts.reduce((s, p) =>
+                        s + p.exercises, 0
+                    )} />
+                </div>)
+             }
+        </div>
+            
+       
+    )
+}
+
+export default Course
